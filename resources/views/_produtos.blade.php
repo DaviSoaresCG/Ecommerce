bbc55d80c2010cab248a48e49391b4f14cd1cad5
davi-scg <davi.gomes2@estudante.ifto.edu.br>
2025-04-29 15:15:18 -0300

    navbar responsivo

--- resources/views/_produtos.blade.php
+++ resources/views/_produtos.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-row flex-wrap items-center justify-center gap-5 m-auto">
+<div class="flex flex-row flex-wrap items-center justify-center gap-5 m-auto mb-4">
     @foreach ($data as $produto)
         <section class="max-w-76 min-w-56 p-6 border-2 border-gray-300 rounded-md">
             <div class="flex items-center justify-center">
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,13 +8,21 @@
     <title>E-Commerce</title>
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     @yield('scriptjs')
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </head>
 
 <body class="text-black">
     <header class="bg-gray-100 p-7">
-        <nav class="flex flex-row space-x-5">
+        <nav class="flex flex-row justify-between space-x-5">
+
             <a href="#" class="text-2xl text-orange-500">Davi S.A</a>
-            <ul class="flex flex-row flex-wrap space-x-3 items-center justify-center text-sm md:text-base lg:text-lg">
+            <span class="cursor-pointer sm:hidden block m-0 p-0">
+                <ion-icon name="menu" onclick="Menu(this)"></ion-icon>
+            </span>
+
+            <ul
+                class="flex flex-col sm:flex-row z-[1] sm:z-auto sm:static absolute space-x-3 sm:items-center justify-center text-sm md:text-base lg:text-lg bg-gray-100 sm:left-auto left-0 sm:mt-auto p-4 sm:p-0 w-full sm:w-auto gap-3 sm:pl-0 pl-7 sm:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
                 <li class="mb-0">
                     <a href="{{ route('home') }}"
                         class="hover:text-orange-500 transition-all duration-100 ease-in-out">Home</a>
@@ -28,6 +36,10 @@
                         <a href="{{ route('cadastrar') }}"
                             class="hover:text-orange-500 transition-all duration-100 ease-in-out">Cadastrar</a>
                     </li>
+                    <li>
+                        <a href="{{ route('ver_carrinho') }}"
+                            class="hover:text-orange-500 transition-all duration-100 ease-in-out">Ver Carrinho</a>
+                    </li>
                 @endif
 
                 @if (!Auth::user())
@@ -37,8 +49,14 @@
                     </li>
                 @else
                     <li>
-                        <a href="{{route('historico')}}" class="hover:text-orange-500 transition-all duration-100 ease-in-out">Minhas
+                        <a href="{{ route('historico') }}"
+                            class="hover:text-orange-500 transition-all duration-100 ease-in-out">Minhas
                             Compras</a>
+                    </li>
+
+                    <li>
+                        <a href="{{ route('ver_carrinho') }}"
+                            class="hover:text-orange-500 transition-all duration-100 ease-in-out">Ver Carrinho</a>
                     </li>
                     <li>
                         <a href="{{ route('logout') }}"
@@ -47,20 +65,6 @@
                 @endif
 
             </ul>
-            
-
-            <div class="ml-auto flex flex-row gap-x-10 justify-center items-center">
-                @if (Auth::user())
-                    <p>Olá, {{ Auth::user()->nome }}</p>
-                @endif
-                <a href="{{ route('ver_carrinho') }}" class="cursor-pointer">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="size-6 hover:text-orange-500 transition-all duration-200 ease">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                    </svg>
-                </a>
-            </div>
 
         </nav>
     </header>
@@ -79,5 +83,23 @@
         @yield('conteudo')
     </main>
 </body>
+<script>
+    function Menu(e) {
+        let list = document.querySelector('ul');
+
+        if (e.name === 'menu') {
+            e.name = "close";
+            list.classList.remove('top-[-400px]');
+            list.classList.add('top-[80px]');
+            list.classList.add('opacity-100');
+        } else {
+            e.name = "menu";
+            list.classList.remove('top-[80px]');
+            list.classList.remove('opacity-100');
+            list.classList.add('top-[-400px]');
+        }
+    }
+</script>
+
 
 </html>

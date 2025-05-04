588329415c9d563c13b1c93051cd11f2b7c41663
davi-scg <davi.gomes2@estudante.ifto.edu.br>
2025-05-04 17:02:19 -0300

    Jobs Emails

---
+++ app/Jobs/MailJob.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\MyTesteEmail;
+use App\Models\Usuario;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Mail;
+
+class MailJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(Private Usuario $user)
+    {
+
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Mail::to($this->user->email)->send(New MyTesteEmail($this->user->nome));
+    }
+}
